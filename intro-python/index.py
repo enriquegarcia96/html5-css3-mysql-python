25de387243bddf273449b019b626d539276223ba
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-07-26 14:28:39 -0600

    python desde 0

---
+++ intro-python/index.py
@@ -0,0 +1,30 @@
+# acá va a un comentario
+if 3 > 5: # acá va a un comentario
+    print('Esto no se va a imprimir')
+
+# if 5 > 3: # acá va a otro comentario
+#     print('5 es mayor a 3')
+
+x = 5
+y = 'chanchito feliz'
+
+print(x, y)
+
+email = 'enrique@gmail.com'
+
+print(email)
+
+mi_var = 'chanchito'
+MIVAR = 'chanchito'
+a , b , c = 'kike', 'diana', 'marleny'
+
+print(a,b, c)
+
+valor1 = valor2 = valor3 = 'chanchito feliz'
+print(valor1, valor2, valor3)
+
+# concatenacion
+inicio = 'Hola '
+final = 'mundo'
+
+print(inicio + final)
